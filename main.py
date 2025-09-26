9d9e71187302c5ac1dcb6931ff6b2c57cc8745a5
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-26 23:06:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Madlibs
+# Word game where you crate a story
+# By filling in blanks with random words
+# Try this!!
+
+adjective1 = input("Enter an adjective (description) : ")
+noun1 = input ("Enter an noun(person,place,thing): ")
+adjective2 = input("Enter an adjective (description) : ")
+verb1 = input ("Enter an verb ending with 'ing' ")
+adjective3 = input("Enter an adjective (description) : ")
+
+
+print(f"Today I went to a {adjective1}")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2}and{verb1}")
+print (f"I was {adjective3}!")
+
+
+
